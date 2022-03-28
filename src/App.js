e5a1e93627272afb138116c21fdfd39cb56257ca
Shabab Qaisar <shababq@themoonshotfactory.com>
2022-03-28 14:24:48 +0500

    compleat crud opration using reduxtool/kit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
+import "./App.css";
+import { addUser, deleteUser, updateUser } from "./features/Users";
+import { Button } from "react-bootstrap";
+import "react-bootstrap";
 
 function App() {
+  const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value);
+  const [name, setName] = useState("");
+  const [username, setUserName] = useState("");
+  const [newuser , setNewName ] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div> </div>
+      <div className="addUser">
+        <input
+          type={"text"}
+          placeholder={"Name"}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        ></input>
+        <input
+          type={"text"}
+          placeholder={"userName"}
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+        ></input>
+        <Button
+          variant="primary"
+          onClick={() => {
+            dispatch(
+              addUser({
+                id: userList[userList.length - 1].id + 1,
+                name,
+                username,
+              })
+            );
+            console.log(name, username);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add User
+        </Button>
+      </div>
+      <div className="displayUsers">
+        {userList.map((user, index) => {
+          return (
+            <div>
+              <label key={index - 1}>{user.id}</label>
+              <h1 key={index}>{user.name}</h1>
+              <h1 key={index + 1}>{user.username}</h1>
+              <input
+                type={"text"}
+                placeholder={"NewuserName"}
+                onChange={(e) => {
+                  setNewName(e.target.value);
+                }}
+              ></input>
+              <button
+                onClick={() => {
+                  console.log("userUpdated");
+                  dispatch(updateUser({ id: user.id , username : newuser }));
+                }}
+              >
+                Update userName
+              </button>
+              <button
+                onClick={() => {
+                  dispatch(deleteUser({ id: user.id }));
+                  console.log(dispatch(deleteUser({ id: user.id })));
+                }}
+              >
+                Delete User
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/FakeData.js
@@ -0,0 +1,32 @@
+export const UsersData = [
+  {
+    id: 1,
+    name: "umer farooq",
+    username: "iamumer",
+  },
+  {
+    id: 2,
+    name: "umer farooq",
+    username: "iamumer",
+  },
+  {
+    id: 3,
+    name: "umer farooq",
+    username: "iamumer",
+  },
+  {
+    id: 4,
+    name: "umer farooq",
+    username: "iamumer",
+  },
+  {
+    id: 5,
+    name: "umer farooq",
+    username: "iamumer",
+  },
+  {
+    id: 6,
+    name: "umer farooq",
+    username: "iamumer",
+  },
+];
